67b9c25c4ed3a30139c68c6a3d9b59581ccb40b7
wuhuhu-wahaha <61803259+wuhuhu-wahaha@users.noreply.github.com>
2020-03-04 21:35:03 +0100

    test
    test

---
+++ includes/head_section.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Styling for public area -->
+	<link rel="stylesheet" href="static/css/public_styling.css">
+	<meta charset="UTF-8">
